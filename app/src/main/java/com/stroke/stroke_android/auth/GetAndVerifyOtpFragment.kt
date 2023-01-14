ff004389069c09b89be1ea1974d21e014b026133
maw9 <kaungmawaung9999@gmail.com>
2023-01-14 06:44:18 +0630

    design get and verify otp fragment

--- app/src/main/java/com/stroke/stroke_android/auth/GetAndVerifyOtpFragment.kt
+++ app/src/main/java/com/stroke/stroke_android/auth/GetAndVerifyOtpFragment.kt
@@ -1,11 +1,10 @@
 package com.stroke.stroke_android.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.stroke.stroke_android.R
+import androidx.fragment.app.Fragment
 import com.stroke.stroke_android.databinding.FragmentGetAndVerifyOtpBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,8 +24,19 @@ class GetAndVerifyOtpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val viewPagerAdapter = AuthScreenPagerAdapter(this)
+        val viewPagerAdapter = AuthScreenPagerAdapter(this) {
+            binding.vpGetPhoneAndOtp.currentItem = 1
+        }
+
         binding.vpGetPhoneAndOtp.adapter = viewPagerAdapter
+
+        binding.vpGetPhoneAndOtp.isUserInputEnabled = false
+
+        binding.btnBack.setOnClickListener {
+            if (binding.vpGetPhoneAndOtp.currentItem == 1) {
+                binding.vpGetPhoneAndOtp.currentItem = 0
+            }
+        }
 
     }
 
--- app/src/main/java/com/stroke/stroke_android/auth/tabs/verify_otp/VerifyOtpFragment.kt
+++ app/src/main/java/com/stroke/stroke_android/auth/tabs/verify_otp/VerifyOtpFragment.kt
@@ -1,19 +1,31 @@
 package com.stroke.stroke_android.auth.tabs.verify_otp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.stroke.stroke_android.R
+import androidx.fragment.app.Fragment
+import com.stroke.stroke_android.databinding.FragmentVerifyOtpBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class VerifyOtpFragment : Fragment() {
+@AndroidEntryPoint
+class VerifyOtpFragment(private val phoneNumber: String) : Fragment() {
+
+    private lateinit var binding: FragmentVerifyOtpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_verify_otp, container, false)
+    ): View {
+        return FragmentVerifyOtpBinding.inflate(inflater, container, false)
+            .also { binding = it }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvBody.text = "We have sent OTP code on your mobile number ($phoneNumber)"
+
     }
 
 }
