491e55f68cf91027730f7b20547fff9814b9c972
Aung Khant Phyo <aungkhantphyo9999@gmail.com>
2023-01-30 21:19:39 +0630

    load image with glide function added

--- app/src/main/java/com/stroke/stroke_android/commonKotlin/ViewExtensionFun.kt
+++ app/src/main/java/com/stroke/stroke_android/commonKotlin/ViewExtensionFun.kt
@@ -1,2 +1,11 @@
 package com.stroke.stroke_android.commonKotlin
 
+import android.net.Uri
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+fun ImageView.loadImageWithGlideWithUri(uri: Uri?){
+    uri?.let {
+        Glide.with(this).load(it).circleCrop().into(this)
+    }
+}
